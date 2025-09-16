4b379c11bec0d7959edc420f1786d19a0fa07083
dnlt707 <128436719+Danielit707@users.noreply.github.com>
2025-09-16 11:04:42 -0500

    Create settings.py

---
+++ foro/settings.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME', 'foro_db'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
